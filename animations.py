68f3ae9c6bd26ed5db8ae941ee9c7f04d61aa948
liam-murphy14 <liam.murphy137@gmail.com>
2023-11-30 19:27:14 -0800

    added controls for n and sleep time

--- animations.py
+++ animations.py
@@ -8,20 +8,21 @@ Outline
 5. show double ended bfs
 """
 import tkinter as tk
+from tkinter import ttk
 from collections import deque
 import time
 
 # ACTUAL ALGOS
 
 
-def _is_valid_position(x, y, n):
+def _is_valid_position(x: int, y: int, n: int) -> bool:
     """
     Check if position is valid.
     """
     return 0 <= x < n and 0 <= y < n
 
 
-def _get_bishop_positions(bishop_x, bishop_y, n):
+def _get_bishop_positions(bishop_x: int, bishop_y: int, n: int) -> set[tuple[int, int]]:
     """
     Get all possible positions for the bishop.
     """
@@ -49,7 +50,7 @@ def bfs(
     bishop_y: int,
     chess_frame: tk.Canvas | None = None,
     sleep_time: float = 0,
-    counter: tk.Label | None = None,
+    counter: tk.StringVar | None = None,
     debug: bool = False,
 ) -> int:
     """
@@ -64,6 +65,9 @@ def bfs(
     queue.append((start_x, start_y, 0, True))
     num_visited = 0
     while queue:
+        if not running:
+            return -1
+
         x, y, distance, is_bishop_alive = queue.popleft()
 
         # check if we found the end
@@ -72,7 +76,7 @@ def bfs(
                 chess_frame.itemconfig(chess_squares[x][y], fill=GREY)
                 # TODO: move knight here and show path ? potentially
                 chess_frame.update()
-                time.sleep(sleep_time)
+                window.after(int(sleep_time * 1000))
             if debug:
                 print(f"Found after {num_visited} nodes")
             return distance
@@ -89,7 +93,7 @@ def bfs(
         if debug:
             print(f"Visiting {(x, y, is_bishop_alive)}")
         if counter is not None:
-            counter.config(text=f"Visited: {num_visited} nodes")
+            counter.set(f"Visited: {num_visited} nodes")
         if chess_frame is not None:
             chess_frame.itemconfig(chess_squares[x][y], fill=GREY)
             chess_frame.update()
@@ -176,119 +180,192 @@ def dbfs(
 
 # TKINTER ANIMATIONS
 
+
+def _reset_chess_board() -> tuple[int, int, int]:
+    """
+    Reset chess board to original state.
+    """
+    global chess_squares
+    chess_squares = []
+    bishop_positions = _get_bishop_positions(bishop_x.get(), bishop_y.get(), n.get())
+    for i in range(n.get()):
+        chess_row = []
+        for j in range(n.get()):
+            color = (
+                RED
+                if (i, j) in bishop_positions
+                else WHITE
+                if (i + j) % 2 == 0
+                else GREEN
+            )
+            x1, y1, x2, y2 = (
+                j * CHESS_SIZE / n.get(),
+                i * CHESS_SIZE / n.get(),
+                (j + 1) * CHESS_SIZE / n.get(),
+                (i + 1) * CHESS_SIZE / n.get(),
+            )
+            square_frame = chess_frame.create_rectangle(
+                x1,
+                y1,
+                x2,
+                y2,
+                fill=color,
+            )
+            chess_row.append(square_frame)
+        chess_squares.append(chess_row)
+    # images
+    SQUARE_WIDTH = CHESS_SIZE / n.get()
+
+    unscaled_knight_img = tk.PhotoImage(file="knight.png")
+    scale_factor = int(unscaled_knight_img.width() / SQUARE_WIDTH) + 1
+    global knight_img
+    knight_img = unscaled_knight_img.subsample(scale_factor, scale_factor)
+
+    unscaled_bishop_img = tk.PhotoImage(file="bishop.png")
+    scale_factor = int(unscaled_bishop_img.width() / SQUARE_WIDTH) + 1
+    global bishop_img
+    bishop_img = unscaled_bishop_img.subsample(scale_factor, scale_factor)
+
+    unscaled_king_img = tk.PhotoImage(file="king.png")
+    scale_factor = int(unscaled_king_img.width() / SQUARE_WIDTH) + 1
+    global king_img
+    king_img = unscaled_king_img.subsample(scale_factor, scale_factor)
+
+    knight_image_id = chess_frame.create_image(
+        start_y.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        start_x.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        image=knight_img,
+    )
+    king_image_id = chess_frame.create_image(
+        end_y.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        end_x.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        image=king_img,
+    )
+    bishop_img_id = chess_frame.create_image(
+        bishop_y.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        bishop_x.get() * CHESS_SIZE / n.get() + CHESS_SIZE / (2 * n.get()),
+        image=bishop_img,
+    )
+    chess_frame.update()
+    return knight_image_id, bishop_img_id, king_image_id
+
+
+def _start():
+    """
+    Start the animation loop
+    """
+    run_buttons_frame.pack_forget()
+    cancel_buttons_frame.pack(pady=20)
+    global running
+    running = True
+
+
+def _stop():
+    """
+    Stop the animation loop
+    """
+    global running
+    running = False
+    cancel_buttons_frame.pack_forget()
+    run_buttons_frame.pack(pady=20)
+    _reset_chess_board()
+    counter_text.set("Visited: 0 nodes")
+
+
+def _start_bfs():
+    """
+    Start the BFS animation loop
+    """
+    _start()
+    bfs(
+        start_x.get(),
+        start_y.get(),
+        end_x.get(),
+        end_y.get(),
+        n.get(),
+        bishop_x.get(),
+        bishop_y.get(),
+        chess_frame,
+        sleep_time.get(),
+        counter_text,
+    )
+
+
 GREEN = "#B3C7A5"
 RED = "#AE4D5B"
 WHITE = "#FEF9EB"
 GREY = "#C0C0C0"
 
-# TODO: add interactivity here
-n = 16
-start_pos = (4, 2)
-end_pos = (2, 6)
-bishop_pos = (2, 3)
 
 window = tk.Tk()
 window.title("Knight Moves")
-window.geometry("1200x700")  # TODO: make responsive/ no hardcoding
+content = ttk.Frame(window)  # TODO: make responsive/ no hardcoding
+content.pack()
 CHESS_SIZE = 650  # TODO: make responsive/ no hardcoding
-CHESS_MARGIN = 25  # TODO: make responsive/ no hardcoding
+MARGIN = 25  # TODO: make responsive/ no hardcoding
+
+# defaults
+n = tk.IntVar(value=8)
+n.trace_add("write", lambda *_: _reset_chess_board())
+start_x = tk.IntVar(value=4)
+start_y = tk.IntVar(value=2)
+end_x = tk.IntVar(value=2)
+end_y = tk.IntVar(value=6)
+bishop_x = tk.IntVar(value=2)
+bishop_y = tk.IntVar(value=3)
+sleep_time = tk.DoubleVar(value=0.1)
 
 # render chess board
-chess_frame = tk.Canvas(window, width=CHESS_SIZE, height=CHESS_SIZE)
-chess_frame.place(x=CHESS_MARGIN, y=CHESS_MARGIN)
-chess_squares = []  # list of lists of rectangle ids
-for i in range(n):
-    chess_row = []
-    for j in range(n):
-        color = WHITE if (i + j) % 2 == 0 else GREEN
-        x1, y1, x2, y2 = (
-            j * CHESS_SIZE / n,
-            i * CHESS_SIZE / n,
-            (j + 1) * CHESS_SIZE / n,
-            (i + 1) * CHESS_SIZE / n,
-        )
-        square_frame = chess_frame.create_rectangle(
-            x1,
-            y1,
-            x2,
-            y2,
-            fill=color,
-        )
-        chess_row.append(square_frame)
-    chess_squares.append(chess_row)
+chess_frame = tk.Canvas(content, width=CHESS_SIZE, height=CHESS_SIZE)
+chess_frame.grid(row=0, column=0, padx=MARGIN, pady=MARGIN)
 
-# images
-SQUARE_WIDTH = CHESS_SIZE / n
 
-unscaled_knight_img = tk.PhotoImage(file="knight.png")
-scale_factor = int(unscaled_knight_img.width() / SQUARE_WIDTH) + 1
-knight_img = unscaled_knight_img.subsample(scale_factor, scale_factor)
-
-unscaled_bishop_img = tk.PhotoImage(file="bishop.png")
-scale_factor = int(unscaled_bishop_img.width() / SQUARE_WIDTH) + 1
-bishop_img = unscaled_bishop_img.subsample(scale_factor, scale_factor)
-
-unscaled_king_img = tk.PhotoImage(file="king.png")
-scale_factor = int(unscaled_king_img.width() / SQUARE_WIDTH) + 1
-king_img = unscaled_king_img.subsample(scale_factor, scale_factor)
-
-chess_frame.create_image(
-    start_pos[1] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    start_pos[0] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    image=knight_img,
-)
-chess_frame.create_image(
-    end_pos[1] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    end_pos[0] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    image=king_img,
-)
-chess_frame.create_image(
-    bishop_pos[1] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    bishop_pos[0] * CHESS_SIZE / n + CHESS_SIZE / (2 * n),
-    image=bishop_img,
-)
-
-bishop_positions = _get_bishop_positions(bishop_pos[0], bishop_pos[1], n)
-for x, y in bishop_positions:
-    chess_frame.itemconfig(chess_squares[x][y], fill=RED)
-chess_frame.update()
+(
+    knight_image_id,
+    bishop_img_id,
+    king_image_id,
+) = _reset_chess_board()  # list of lists of rectangle ids
 
 # render buttons and counter
-button_frame = tk.Frame(window, width=500, height=700)
-button_frame.place(x=700, y=0)
-counter = tk.Label(button_frame, text="0", font=("Arial", 50))
-counter.pack(expand=True, fill=tk.X)
-bfs_button = tk.Button(
-    button_frame,
+control_frame = ttk.Frame(content)
+control_frame.grid(row=0, column=1, padx=(0, MARGIN), pady=MARGIN)
+
+counter_text = tk.StringVar(control_frame, value="Visited: 0 nodes")
+counter = ttk.Label(control_frame, textvariable=counter_text, font=("Arial", 44))
+counter.pack()
+
+run_buttons_frame = ttk.Frame(control_frame)
+bfs_button = ttk.Button(
+    run_buttons_frame,
     text="Run BFS",
-    command=lambda: bfs(
-        start_pos[0],
-        start_pos[1],
-        end_pos[0],
-        end_pos[1],
-        n,
-        bishop_pos[0],
-        bishop_pos[1],
-        chess_frame,
-        0.1,
-    ),
+    command=lambda: _start_bfs(),
 )
-bfs_button.pack(expand=True, fill=tk.X)
-dbfs_button = tk.Button(
-    button_frame,
-    text="Run Double BFS",
-    command=lambda: dbfs(
-        start_pos[0],
-        start_pos[1],
-        end_pos[0],
-        end_pos[1],
-        n,
-        bishop_pos[0],
-        bishop_pos[1],
-        chess_frame,
-    ),
+bfs_button.grid(row=0, column=0, padx=10)
+dbfs_button = ttk.Button(
+    run_buttons_frame, text="Run Double BFS", command=lambda: print("hi")
 )
-dbfs_button.pack(expand=True, fill=tk.X)
+dbfs_button.grid(row=0, column=1, padx=10)
+run_buttons_frame.pack(pady=20)
+
+cancel_buttons_frame = ttk.Frame(control_frame)
+cancel_button = ttk.Button(
+    cancel_buttons_frame,
+    text="Reset",
+    command=_stop,
+)
+cancel_button.pack()
+
+run_controls_frame = ttk.Frame(control_frame)
+n_label = ttk.Label(run_controls_frame, text="Chess board size:")
+n_entry = ttk.Entry(run_controls_frame, textvariable=n, width=6)
+n_label.grid(row=0, column=0, padx=10)
+n_entry.grid(row=0, column=1, padx=10)
+tick_speed_label = ttk.Label(run_controls_frame, text="Tick duration:")
+tick_speed_entry = ttk.Entry(run_controls_frame, textvariable=sleep_time, width=6)
+tick_speed_label.grid(row=1, column=0, padx=10)
+tick_speed_entry.grid(row=1, column=1, padx=10)
+run_controls_frame.pack(pady=20)
+
 
 window.mainloop()
 
